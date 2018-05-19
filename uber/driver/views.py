a86a8ab3ad026c707104ea554e260ff647b0b3f7
mark <murimimg180@gmail.com>
2018-05-19 20:04:17 +0300

    made changes

--- uber/driver/views.py
+++ uber/driver/views.py
@@ -1,20 +1,26 @@
 from django.http  import HttpResponse
 from django.shortcuts import render
 from .models import Driver
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
 def welcome(request):
     return render(request, 'welcome.html')
 
+@login_required(login_url='/accounts/login/')
 def home(request):
     return render(request, 'home.html')
 
+@login_required(login_url='/accounts/login/')
 def d_profile(request):
     return render(request, 'driver-profile.html')
 
+@login_required(login_url='/accounts/login/')
 def home2(request):
     return render(request, 'home2.html')
 
+@login_required(login_url='/accounts/login/')
 def p_profile(request):
     return render(request, 'passenger-profile.html')
 
